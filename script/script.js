e042d13f8ec0a71ae58307ea6a5405ac955971a9
Nataniel Malysh <114068925+Natan2511@users.noreply.github.com>
2024-04-06 19:01:49 +0300

    fix bag

--- script/script.js
+++ script/script.js
@@ -1,44 +1,10 @@
-const test = [
-  {
-    name: "Yair Rodríguez",
-    text: "¡Realmente es bueno! Llevo unas semanas tomando D-Norm. No me he sentido así en mucho tiempo. Mis niveles de azúcar volvieron a la normalidad al día siguiente de tomarlo por primera vez, ¡qué pasará cuando haya tomado todo el curso!",
-    image: "",
-    timestamp: "2024-04-06T13:40:10.333Z",
-  },
-];
-const sample = [
-  {
-    name: "Elizabeth Vargas",
-    text: "Esta es la verdad sobre lo que hay detrás de las compañías farmacéuticas. Todo lo que quieren de nosotros es dinero. Recomiendan suplementos que son ineficaces y sólo dan falsas esperanzas. No recetan suplementos que realmente ayudan, simplemente no los tienen en las farmacias. Me preocupo por mi país. Nadie se preocupa por los diabéticos. Es triste que no pueda vivir en Europa. Gracias por compartir la información.",
-    image: "",
-    timestamp: "2024-04-06T13:40:10.333Z",
-  },
-  {
-    name: "Maribel Peña",
-    text: "Soy diabética desde hace mucho tiempo (más de 16 años), he probado de todo para normalizar mi azúcar, quitar el adormecimiento de las manos y la sed excesiva. Bebí varias infusiones de hierbas, seguí dietas estrictas, compré todos los productos que se ofrecían en la farmacia, pero el resultado fue insignificante. Sólo vi un efecto real con D-Norm. No me he sentido tan bien en años. Mi nivel de azúcar en sangre ha dejado de aumentar, puedo olvidarme de las restricciones dietéticas estrictas (¡por fin!), incluso he dejado de llevar una botella de agua de 1,5 litros a todos lados porque ahora no quiero beberla todo el tiempo. Sólo quiero dar las gracias a los creadores de D-Norm, ¡han revolucionado la lucha contra diabetes de tipo 2!",
-    image: "",
-    timestamp: "2024-04-06T13:40:10.333Z",
-  },
-  {
-    name: "Carmela Espinoza",
-    text: "Usé D-Norm hace dos años. Lo pedí en la página web oficial. Desde hace 2 años me siento como una persona sana. De todas las cosas que he probado (soy diabética desde hace mucho tiempo), D-Norm es el mejor suplemento. ¡No lo dudes! ¡Es tu salvador de las enfermedades!",
-    image: "",
-    timestamp: "2024-04-06T13:40:10.333Z",
-  },
-  {
-    name: "Mario Pacheco",
-    text: "Pedí D-Norm. Vivo en Oaxaca y me prometieron la entrega en una semana. Estoy deseando que llegue.",
-    image: "",
-    timestamp: "2024-04-06T13:40:10.333Z",
-  },
-];
-
 document.addEventListener("DOMContentLoaded", function () {
   autoResizeTextarea();
 
   const storedComments = JSON.parse(localStorage.getItem("comments")) || [];
   const commentsList = document.getElementById("commentsList");
 
+  // Добавляем сохраненные комментарии на страницу
   storedComments.forEach(function (comment) {
     const newComment = createCommentElement(
       comment.name,
@@ -49,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     commentsList.appendChild(newComment);
   });
 
-  let currentIndex = 0;
+  let currentIndex = storedComments.length;
   let scrolledToComments = false;
 
   window.addEventListener("scroll", function () {
@@ -59,13 +25,46 @@ document.addEventListener("DOMContentLoaded", function () {
     if (commentsWrapperPosition <= window.innerHeight && !scrolledToComments) {
       scrolledToComments = true;
       setTimeout(function () {
-        const newComment = createCommentElement(
-          test[0].name,
-          test[0].text,
-          test[0].image,
-          new Date(test[0].timestamp)
-        );
-        commentsList.appendChild(newComment);
+        if (currentIndex < sample.length) {
+          const newCommentData = sample[currentIndex];
+          const newComment = createCommentElement(
+            newCommentData.name,
+            newCommentData.text,
+            newCommentData.image,
+            new Date()
+          );
+          commentsList.insertAdjacentHTML(
+            "afterbegin",
+            `
+            <li class="list">
+              <label class="avatar" for="fileInputAvatar">
+                <img src="./images/user.png" alt="Upload Image" width="40" height="40"/>
+              </label>
+              <span class="name">${newCommentData.name}</span>
+              <p class="text">${newCommentData.text}</p>
+              ${
+                newCommentData.image
+                  ? `<div class="text-img">${newCommentData.image}</div>`
+                  : ""
+              }
+              <p class="date">Published: ${new Date().toLocaleString()}</p>
+              <div class="line"></div>
+            </li>
+          `
+          );
+
+          storedComments.unshift({
+            // Сохраняем новый комментарий в начало массива
+            id: Date.now().toString(), // Генерируем уникальный id
+            name: newCommentData.name,
+            text: newCommentData.text,
+            image: newCommentData.image,
+            timestamp: new Date().toISOString(),
+          });
+          localStorage.setItem("comments", JSON.stringify(storedComments));
+
+          currentIndex++;
+        }
       }, 2000);
     }
   });
@@ -73,16 +72,39 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(function () {
     if (currentIndex < sample.length) {
       const newCommentData = sample[currentIndex];
-      const newComment = createCommentElement(
-        newCommentData.name,
-        newCommentData.text,
-        newCommentData.image,
-        new Date()
+      commentsList.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <li class="list">
+          <label class="avatar" for="fileInputAvatar">
+            <img src="./images/user.png" alt="Upload Image" width="40" height="40"/>
+          </label>
+          <span class="name">${newCommentData.name}</span>
+          <p class="text">${newCommentData.text}</p>
+          ${
+            newCommentData.image
+              ? `<div class="text-img">${newCommentData.image}</div>`
+              : ""
+          }
+          <p class="date">Published: ${new Date().toLocaleString()}</p>
+          <div class="line"></div>
+        </li>
+      `
       );
-      commentsList.appendChild(newComment);
+
+      storedComments.unshift({
+        // Сохраняем новый комментарий в начало массива
+        id: Date.now().toString(), // Генерируем уникальный id
+        name: newCommentData.name,
+        text: newCommentData.text,
+        image: newCommentData.image,
+        timestamp: new Date().toISOString(),
+      });
+      localStorage.setItem("comments", JSON.stringify(storedComments));
+
       currentIndex++;
     }
-  }, 20 * 1000); // 20 секунд * 1000 миллисекунд
+  }, 20 * 1000);
 });
 
 //! textarea
@@ -151,7 +173,7 @@ document.querySelector(".form").addEventListener("submit", function (event) {
     const commentsList = document.getElementById("commentsList");
     const timestamp = new Date();
     const newComment = createCommentElement(name, comment, image, timestamp);
-    commentsList.appendChild(newComment);
+    commentsList.insertBefore(newComment, commentsList.firstChild); // Вставляем новый комментарий перед первым элементом в списке
 
     nameInput.value = "";
     commentInput.value = "";
@@ -164,13 +186,12 @@ document.querySelector(".form").addEventListener("submit", function (event) {
       image: image,
       timestamp: timestamp,
     };
-    storedComments.push(newStoredComment);
+    storedComments.unshift(newStoredComment); // Добавляем новый комментарий в начало массива
     localStorage.setItem("comments", JSON.stringify(storedComments));
   } else {
     alert("Please enter a comment.");
   }
 });
-
 function createCommentElement(name, comment, image, timestamp) {
   const options = {
     year: "numeric",
@@ -195,3 +216,38 @@ function createCommentElement(name, comment, image, timestamp) {
   `;
   return newComment;
 }
+
+const test = [
+  {
+    name: "Yair Rodríguez",
+    text: "¡Realmente es bueno! Llevo unas semanas tomando D-Norm. No me he sentido así en mucho tiempo. Mis niveles de azúcar volvieron a la normalidad al día siguiente de tomarlo por primera vez, ¡qué pasará cuando haya tomado todo el curso!",
+    image: "",
+    timestamp: "2024-04-06T13:40:10.333Z",
+  },
+];
+const sample = [
+  {
+    name: "Elizabeth Vargas",
+    text: "Esta es la verdad sobre lo que hay detrás de las compañías farmacéuticas. Todo lo que quieren de nosotros es dinero. Recomiendan suplementos que son ineficaces y sólo dan falsas esperanzas. No recetan suplementos que realmente ayudan, simplemente no los tienen en las farmacias. Me preocupo por mi país. Nadie se preocupa por los diabéticos. Es triste que no pueda vivir en Europa. Gracias por compartir la información.",
+    image: "",
+    timestamp: "2024-04-06T13:40:10.333Z",
+  },
+  {
+    name: "Maribel Peña",
+    text: "Soy diabética desde hace mucho tiempo (más de 16 años), he probado de todo para normalizar mi azúcar, quitar el adormecimiento de las manos y la sed excesiva. Bebí varias infusiones de hierbas, seguí dietas estrictas, compré todos los productos que se ofrecían en la farmacia, pero el resultado fue insignificante. Sólo vi un efecto real con D-Norm. No me he sentido tan bien en años. Mi nivel de azúcar en sangre ha dejado de aumentar, puedo olvidarme de las restricciones dietéticas estrictas (¡por fin!), incluso he dejado de llevar una botella de agua de 1,5 litros a todos lados porque ahora no quiero beberla todo el tiempo. Sólo quiero dar las gracias a los creadores de D-Norm, ¡han revolucionado la lucha contra diabetes de tipo 2!",
+    image: "",
+    timestamp: "2024-04-06T13:40:10.333Z",
+  },
+  {
+    name: "Carmela Espinoza",
+    text: "Usé D-Norm hace dos años. Lo pedí en la página web oficial. Desde hace 2 años me siento como una persona sana. De todas las cosas que he probado (soy diabética desde hace mucho tiempo), D-Norm es el mejor suplemento. ¡No lo dudes! ¡Es tu salvador de las enfermedades!",
+    image: "",
+    timestamp: "2024-04-06T13:40:10.333Z",
+  },
+  {
+    name: "Mario Pacheco",
+    text: "Pedí D-Norm. Vivo en Oaxaca y me prometieron la entrega en una semana. Estoy deseando que llegue.",
+    image: "",
+    timestamp: "2024-04-06T13:40:10.333Z",
+  },
+];
